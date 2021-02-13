bf90379d0e6ea5dbe0fe4b05e488acbfc6558417
cti1650 <cti1650@gmail.com>
2021-02-14 00:22:11 +0900

    sos

--- sos/js/app.js
+++ sos/js/app.js
@@ -1,16 +1,16 @@
 function sound() {
   // 対象となるID名
-  var id = 'sound-file';
+  var id = "sound-file";
 
   // 初回以外だったら音声ファイルを巻き戻す
-  if (typeof document.getElementById(id).currentTime != 'undefined') {
+  if (typeof document.getElementById(id).currentTime != "undefined") {
     document.getElementById(id).currentTime = 0;
   }
 
   // [ID:sound-file]の音声ファイルを再生[play()]する
   document.getElementById(id).play();
 }
-document.body.addEventListener('keypress', onKeyPress);
+document.body.addEventListener("keypress", onKeyPress);
 function onKeyPress(e) {
   if (
     e.keyCode === 13 ||
@@ -35,18 +35,79 @@ function onKeyPress(e) {
   sound();
 }
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
+function sos_on() {
+  document.body.style.backgroundColor = "white";
+}
+function sos_off() {
+  document.body.style.backgroundColor = "black";
+}
+
+function sos() {
+  let i = 0;
+  let t = setInterval(function () {
+    console.log(i);
+    switch (i) {
+      case 1:
+      case 3:
+      case 5:
+      case 7:
+      case 12:
+      case 17:
+      case 22:
+      case 24:
+      case 26:
+        sos_on();
+        break;
+      case 2:
+      case 4:
+      case 6:
+      case 11:
+      case 16:
+      case 21:
+      case 23:
+      case 25:
+      case 27:
+        sos_off();
+        break;
+      case 28:
+        clearInterval(t);
+        break;
+      default:
+    }
+    i += 1;
+  }, 200);
+}
+
+function sleep(waitSec, callbackFunc) {
+  var spanedSec = 0;
+
+  var waitFunc = function () {
+    spanedSec++;
+
+    if (spanedSec >= waitSec) {
+      if (callbackFunc) callbackFunc();
+      return;
+    }
+
+    clearTimeout(id);
+    id = setTimeout(waitFunc, 1000);
+  };
+
+  var id = setTimeout(waitFunc, 1000);
+}
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register('sw.js')
+      .register("sw.js")
       .then((reg) => {
         console.log(
-          'サービスワーカーの登録が以下のスコープで完了! ',
+          "サービスワーカーの登録が以下のスコープで完了! ",
           reg.scope
         );
       })
       .catch((err) => {
-        return console.log('サービスワーカーの登録が失敗: ', err);
+        return console.log("サービスワーカーの登録が失敗: ", err);
       });
   });
 }
